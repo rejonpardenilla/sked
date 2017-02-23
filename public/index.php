fe0b42130e897b22d11084fc6f961ac7003b5ee4
David Hernández <davidhernandeze@gmail.com>
2017-02-22 18:46:47 -0600

    Change to sked directory

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../sked/bootstrap/autoload.php';
+require __DIR__.'/../backend/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'/../sked/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../sked/bootstrap/app.php';
+$app = require_once __DIR__.'/../backend/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
